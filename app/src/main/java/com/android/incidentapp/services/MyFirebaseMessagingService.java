cdb8b838da963cf7073b0804bef805eb5073c874
Abhishek Shanthkumar <abhishek.shanthkumar@gmail.com>
2017-01-15 21:46:55 +0530

    Added notification backend.

---
+++ app/src/main/java/com/android/incidentapp/services/MyFirebaseMessagingService.java
@@ -0,0 +1,26 @@
+package com.android.incidentapp.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final String TAG = "InciFMService";
+
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " +
+                remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+}
